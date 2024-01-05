22ffdc215fe1009cd53b9ae57ddb54563bdea0ce
TheJesterEnd <kakhniashvililuka@gmail.com>
2024-01-06 03:23:24 +0400

    change function name and make code more prettier

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const p = document.querySelector("#p");
 const parentDiv = document.querySelector(".summary_content");
 
-async function foo() {
+async function fetchData() {
   try {
     let response = await fetch("data.json");
     if (!response.ok) throw new Error("Error");
@@ -13,26 +13,25 @@ async function foo() {
   }
 }
 async function main() {
-  const result = await foo();
+  const result = await fetchData();
   for (let i = 0; i < result.length; i++) {
-    parentDiv.innerHTML += ` <div class="specs ${result[
-      i
-    ].category.toLowerCase()}">
-    <div class="icon text_${result[i].category.toLowerCase()}">
-  <img src="${result[i].icon}" alt = "${result[i].category} photo">
-      <p>${result[i].category}</p>
-    </div>
-    <div class="numbers">
-      <p id="para_${result[i].category}">${result[i].score}</p>
-      <p>/100</p>
-    </div>
+    parentDiv.innerHTML += `
+    <div class="specs ${result[i].category.toLowerCase()}">
+      <div class="icon text_${result[i].category.toLowerCase()}">
+        <img src="${result[i].icon}" alt = "${result[i].category} photo">
+        <p>${result[i].category}</p>
+      </div>
+      <div class="numbers">
+        <p id="para_${result[i].category}">${result[i].score}</p>
+        <p>/100</p>
+      </div>
     </div> `;
   }
 }
 main();
 
 async function calculate() {
-  const data = await foo();
+  const data = await fetchData();
   const totalScore = data.reduce(
     (acc, currentScore) => acc + currentScore.score,
     0
